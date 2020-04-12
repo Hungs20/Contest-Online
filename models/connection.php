f46803c299be42d91c5875136a6843bbb3613c00
Chu Văn Hưng <hungs20.uet@gmail.com>
2020-04-12 18:13:33 +0700

    Update connection.php

--- models/connection.php
+++ models/connection.php
@@ -2,14 +2,14 @@
 $servername = "localhost";
 $username = "hungs20";
 $password = "hung2710";
-
+//mysql://bf4e459bc6e653:40ef37b6@us-cdbr-iron-east-01.cleardb.net/heroku_7b0aa3836e09062?reconnect=true
 class DB
 {
     private static $instance = NULl;
     public static function getInstance() {
       if (!isset(self::$instance)) {
         try {
-          self::$instance = new PDO("mysql:host=localhost;dbname=tracnghiem", "hungs20", "hung2710", [PDO::ATTR_EMULATE_PREPARES => false, 
+          self::$instance = new PDO("mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_7b0aa3836e09062", "bf4e459bc6e653", "40ef37b6", [PDO::ATTR_EMULATE_PREPARES => false, 
        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
           self::$instance->exec("SET NAMES 'utf8'");
 		  
